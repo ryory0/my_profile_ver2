4bfa7fc271349f567a4ef57ea95ea5cc57bb08d1
ryory0 <108258755+ryory0@users.noreply.github.com>
2024-11-22 18:16:50 +0900

    mod12

--- front/my-app/app/works/page.tsx
+++ front/my-app/app/works/page.tsx
@@ -1,31 +1,39 @@
 "use client";
 
-import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
-import Section from '@/components/section'
-import { WorkGridItem } from '@/components/grid-item'
-import thumbInkdrop from '@/public/images/main.png'
-import thumbWalknote from '@/public/images/test_product_2.png'
+import { Container, Box, Heading, SimpleGrid } from '@chakra-ui/react';
+import Section from '@/components/section';
+import { WorkGridItem } from '@/components/grid-item';
+import thumbInkdrop from '@/public/images/main.png';
+import thumbWalknote from '@/public/images/test_product_2.png';
 
 const Works = () => {
-    return (
-        <Container pt={20} maxW="container.md">
-            <Heading as="h3" fontSize={20} mb={4} mt={5}>
-                Works
-            </Heading>
-            <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="Fodo Food" title="Fodo Food" thumbnail={thumbInkdrop}>
-                        A Markdown note-taking app 
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="VizLab" title="VizLab" thumbnail={thumbWalknote}>
-                        Music recomandation app for iOS
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
-    )
-}
+  return (
+    <Container pt={20} maxW="container.md">
+      <Heading as="h3" fontSize={20} mb={4} mt={5}>
+        Works
+      </Heading>
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem
+            workId="fodo-food" // idをworkIdに変更
+            title="Fodo Food"
+            thumbnail={thumbInkdrop}
+          >
+            A Markdown note-taking app
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem
+            workId="vizlab" // idをworkIdに変更
+            title="VizLab"
+            thumbnail={thumbWalknote}
+          >
+            Music recommendation app for iOS
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+    </Container>
+  );
+};
 
-export default Works
+export default Works;
