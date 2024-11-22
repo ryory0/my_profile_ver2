ef4d9eae2f543d269bf0b6b52a58b6fe8a4fe392
ryory0 <108258755+ryory0@users.noreply.github.com>
2024-11-22 21:14:24 +0900

    mod18

--- front/my-app/next.config.mjs
+++ front/my-app/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,  // Strict Modeを無効化
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: false, // Strict Modeを無効化
+  experimental: {
+    appDir: true, // App Routerを有効化
+  },
+};
+
+export default nextConfig;
