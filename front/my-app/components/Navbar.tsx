56606eb19dda4d1d61e07b5d40f6c362a285df90
ryory0 <108258755+ryory0@users.noreply.github.com>
2024-11-24 15:33:28 +0900

    test28

--- front/my-app/components/Navbar.tsx
+++ front/my-app/components/Navbar.tsx
@@ -1,48 +1,55 @@
 "use client";
 
-import Logo from './Logo'
-import NextLink from 'next/link'
+import Logo from "./Logo";
+import NextLink from "next/link";
 import {
-    Container,
-    Box,
-    Link,
-    Stack,
-    Heading,
-    Flex,
-    Menu,
-    MenuItem,
-    MenuList,
-    MenuButton,
-    IconButton,
-    useColorModeValue
-} from '@chakra-ui/react'
-import { HamburgerIcon } from '@chakra-ui/icons'
-import ThemeToggleButton from '@/components/Theme-toggle-button'
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import ThemeToggleButton from "@/components/Theme-toggle-button";
 
 // NavbarProps の型定義
 type NavbarProps = {
-    path: string;
-}
+  path: string;
+};
 
 // LinkItemProps の型定義
 type LinkItemProps = {
-    href: string;
-    path: string;
-    children: React.ReactNode;
-}
+  href: string;
+  path: string;
+  children: React.ReactNode;
+};
 
 const LinkItem = ({ href, path, children }: LinkItemProps) => {
-    return (
-        <NextLink href={href} passHref legacyBehavior>
-            <Link
-                p={5}
-            >
-                {children}
-            </Link>
-        </NextLink>
-    )
-}
+  const active = path === href;
+  const inactiveColor = useColorModeValue("gray.600", "whiteAlpha.900");
 
+  return (
+    <NextLink href={href} passHref legacyBehavior>
+      <Link
+        p={5}
+        bg={active ? "glassTeal" : undefined}
+        color={active ? useColorModeValue("#202023", "white") : inactiveColor}
+        fontWeight={active ? "bold" : "normal"} // アクティブなリンクは太字に
+        borderRadius="md" // リンクの背景を少し角丸に
+        _hover={{ textDecoration: "none", bg: useColorModeValue("gray.200", "gray.700") }} // ホバー時のスタイル
+      >
+        {children}
+      </Link>
+    </NextLink>
+  );
+};
 
 const Navbar = ({ path, ...props }: NavbarProps) => {
   return (
@@ -50,35 +57,49 @@ const Navbar = ({ path, ...props }: NavbarProps) => {
       position="fixed"
       as="nav"
       w="100%"
-      bg={useColorModeValue('#ffffff40', '#20202380')}
-      style={{ backdropFilter: 'blur(10px)' }}
+      bg={useColorModeValue("#ffffff40", "#20202380")}
+      style={{ backdropFilter: "blur(10px)" }}
       zIndex={1}
       {...props}
     >
-      <Container maxW="container.md" p={2} display="flex" justifyContent="space-between" alignItems="center">
+      <Container
+        maxW="container.md"
+        p={2}
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Flex align="center" w="100%">
           <Flex align="center" mr={5}>
-            <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            <Heading as="h1" size="lg" letterSpacing={"tighter"}>
               <Logo />
             </Heading>
           </Flex>
           <Stack
-            direction={{ base: 'column', md: 'row' }}
-            display={{ base: 'none', md: 'flex' }}
-            width={{ base: 'full', md: 'auto' }}
+            direction={{ base: "column", md: "row" }}
+            display={{ base: "none", md: "flex" }}
+            width={{ base: "full", md: "auto" }}
             alignItems="center"
             flexGrow={1}
             mt={{ base: 4, md: 0 }}
           >
+            <LinkItem href="/" path={path}>
+              About
+            </LinkItem>
             <LinkItem href="/works" path={path}>
               Works
             </LinkItem>
           </Stack>
           <Box flex={1} textAlign="right">
             <ThemeToggleButton />
-            <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Box ml={2} display={{ base: "inline-block", md: "none" }}>
               <Menu>
-                <MenuButton as={IconButton} icon={<HamburgerIcon />} variant="outline" aria-label="Options" />
+                <MenuButton
+                  as={IconButton}
+                  icon={<HamburgerIcon />}
+                  variant="outline"
+                  aria-label="Options"
+                />
                 <MenuList>
                   <NextLink href="/" passHref legacyBehavior>
                     <MenuItem>About</MenuItem>
