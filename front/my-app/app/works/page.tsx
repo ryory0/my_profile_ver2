9427cf703ea832a8fd86f0dc7dd4edb1e6a13c7c
ryory0 <108258755+ryory0@users.noreply.github.com>
2024-11-24 14:18:56 +0900

    test17

---
+++ front/my-app/app/works/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import Section from "../../components/section"
+import { WorkGridItem } from '../../components/grid-item'
+import thumbInkdrop from '../../public/images/main.png'
+import thumbWalknote from '../../public/images/test_product_2.png'
+
+const Works = () => {
+    return (
+        <Container pt={20} maxW="container.md">
+            <Heading as="h3" fontSize={20} mb={4} mt={5}>
+                Works
+            </Heading>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                <Section>
+                    <WorkGridItem id="Fodo Food" title="Fodo Food" thumbnail={thumbInkdrop}>
+                        A Markdown note-taking app 
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="VizLab" title="VizLab" thumbnail={thumbWalknote}>
+                        Music recomandation app for iOS
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+        </Container>
+    )
+}
+
+export default Works
