6102a59115afb38aa96585d73cce4e74f35d26a3
ryory0 <108258755+ryory0@users.noreply.github.com>
2024-11-24 14:40:19 +0900

    test21

--- front/my-app/components/layouts/Header.tsx
+++ front/my-app/components/layouts/Header.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { useRouter } from 'next/router'; 
-import Head from 'next/head';
-import Navbar from '../Navbar';
-import { Box } from '@chakra-ui/react';
+import { useRouter } from "next/router";
+import Navbar from "../Navbar";
+import { Box } from "@chakra-ui/react";
 
 const Header = () => {
-  const router = useRouter(); // 現在のパスを取得
+  const router = useRouter(); // クライアントサイドで現在のパスを取得
 
   return (
-    <Box as="header"> 
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Ryohei Yoshine - Homepage</title>
-      </Head>
+    <Box as="header">
       <Navbar path={router.asPath} /> {/* path を Navbar に渡す */}
     </Box>
   );
