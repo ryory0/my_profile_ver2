43b831bf4a884c0fdacbd6ac5f4116d6080b073f
ryory0 <108258755+ryory0@users.noreply.github.com>
2024-11-24 14:14:27 +0900

    test16

--- front/my-app/app/layout.tsx
+++ front/my-app/app/layout.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import Header from "@/components/layouts/Header";
-
+import Header from "../components/layouts/Header";
+import theme from '../lib/theme';
 import { ReactNode } from 'react';
 import { AnimatePresence } from 'framer-motion';
 
@@ -14,7 +14,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
       </head>
       <body>
-        <ChakraProvider >
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Header />
           <AnimatePresence mode="sync" initial={true}>
               {children} {/* ページのコンテンツ */}
