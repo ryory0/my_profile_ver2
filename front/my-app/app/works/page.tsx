14fc8c358d018182946831df7a7fb233774ca8bb
ryory0 <108258755+ryory0@users.noreply.github.com>
2024-12-05 06:52:02 +0900

    test38

--- front/my-app/app/works/page.tsx
+++ front/my-app/app/works/page.tsx
@@ -4,7 +4,6 @@ import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from "../../components/section"
 import { WorkGridItem } from '../../components/grid-item'
 import thumbInkdrop from '../../public/images/main.png'
-import thumbWalknote from '../../public/images/test_product_2.png'
 
 const Works = () => {
     return (
@@ -14,13 +13,8 @@ const Works = () => {
             </Heading>
             <SimpleGrid columns={[1, 1, 2]} gap={6}>
                 <Section>
-                    <WorkGridItem id="Fodo Food" title="Fodo Food" thumbnail={thumbInkdrop}>
-                        A Markdown note-taking app 
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="VizLab" title="VizLab" thumbnail={thumbWalknote}>
-                        Music recomandation app for iOS
+                    <WorkGridItem id="Fodo Food" title="Fodo Food" thumbnail={thumbInkdrop} url="https://food-food-kt8rs3xxlppousqfh8jgcg.streamlit.app/">
+                        E-commerce site for local food and cuisine
                     </WorkGridItem>
                 </Section>
             </SimpleGrid>
